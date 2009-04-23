128c8ca7e1eed6a6a74cb5946e517e57af320484
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-23 11:00:20 +0000

    PCoA removes zero eigenvalues (but keeps negative)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mmecor/pkg/barebone@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/PCoA.R
+++ R/PCoA.R
@@ -5,5 +5,8 @@
     M <- scale(M, center = TRUE, scale = FALSE)
     M <- t(scale(t(M), center = TRUE, scale = FALSE))
     SOL <- eigen(-M/2, symmetric = TRUE)
+    nonzero <- abs(SOL$values) > sqrt(.Machine$double.eps)
+    SOL$values <- SOL$values[nonzero]
+    SOL$vectors <- SOL$vectors[, nonzero]
     SOL
 }
