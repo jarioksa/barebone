630872f4b3fad8978aa9d481d82c9b361a04b3d8
psolymos <psolymos@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-10 06:12:56 +0000

    rspecies removed
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mmecor/pkg/barebone@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rspecies.R
+++
@@ -1,7 +0,0 @@
-rspecies <-
-function(n, spp, b=rep(1, spp), x=rep(1, n))
-{
-    log.lambda <- lapply(1:spp, function(z) data.matrix(x) %*% b[[z]])
-    y <- sapply(log.lambda, function(z) rpois(n, exp(z)))
-    y
-}
