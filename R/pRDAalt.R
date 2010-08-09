144d7be81e9788474c80f1765cea14a93025490e
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-09 08:15:20 +0000

    CANOCO-like variant of pRDA: rresiduals of X ~ Z
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mmecor/pkg/barebone@206 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/pRDAalt.R
@@ -0,0 +1,24 @@
+`pRDAalt` <-
+function(Y, X = NULL, Z = NULL, scale = FALSE)
+{
+    Y <- scale(as.matrix(Y), center = TRUE, scale = scale)
+    if (!is.null(Z)) {
+        Z <- scale(as.matrix(Z), center = TRUE, scale = FALSE)
+        QZ <- qr(Z)
+        Y <- qr.resid(QZ, Y)
+    }
+    if (!is.null(X)) {
+        X <- scale(as.matrix(X), center = TRUE, scale = FALSE)
+        if (!is.null(Z))
+            X <- qr.resid(QZ, X)
+        Q <- qr(X)
+        RDA <- svd(qr.fitted(Q, Y))
+        RDA$w <- Y %*% RDA$v %*% diag(1/RDA$d)
+        Y <- qr.resid(Q, Y)
+    } else {
+        RDA <- NULL
+    }
+    RES <- svd(Y)
+    list(RDA = RDA, RES = RES)
+}
+
