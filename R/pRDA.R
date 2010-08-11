4b3be3afda4557bfa54647574b5610a1c0c6ede4
jarioksa <jarioksa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-11 13:30:26 +0000

    pRDA.R more similar to pRDAalt.R
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mmecor/pkg/barebone@207 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/pRDA.R
+++ R/pRDA.R
@@ -4,7 +4,8 @@ function(Y, X = NULL, Z = NULL, scale = FALSE)
     Y <- scale(as.matrix(Y), center = TRUE, scale = scale)
     if (!is.null(Z)) {
         Z <- scale(as.matrix(Z), center = TRUE, scale = FALSE)
-        Y <- qr.resid(qr(Z), Y)
+        QZ <- qr(Z)
+        Y <- qr.resid(QZ, Y)
     }
     if (!is.null(X)) {
         X <- scale(as.matrix(X), center = TRUE, scale = FALSE)
