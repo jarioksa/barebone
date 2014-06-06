d131e8d2908b70def6734f47ee97200b3991806a
Jari Oksanen <jari.oksanen@oulu.fi>
2014-06-06 07:58:46 +0300

    commit a long forgotten edit -- no idea whether this works

--- R/wRDA.R
+++ R/wRDA.R
@@ -1,10 +1,15 @@
 `wRDA` <-
-    function(Y, X = NULL, Z = NULL, scale = FALSE, w = NULL)
+    function(Y, X = NULL, Z = NULL, scale = FALSE, w = NULL, cw = NULL)
 {
     if (is.null(w))
         w <- rep(1/nrow(Y), nrow(Y))
     else
         w <- w/sum(w)
+    if (is.null(cw))
+        cw <- rep(1/ncol(Y), ncol(Y))
+    else
+        cw <- cw/sum(cw)
+    tot <- sum(Y)/(nrow(Y) - 1)
     wscale <- function(x, scale = FALSE, w = NULL) {
         wc <- apply(x, 2, weighted.mean, w = w)
         x <- sweep(x, 2, wc, "-")
@@ -15,6 +20,8 @@
         sweep(x, 1, sqrt(w), "*")
     }
     Y <- wscale(as.matrix(Y), scale = scale, w = w)
+    Y <- sweep(Y, 2, sqrt(cw), "*")
+    Y <- sqrt(tot) * Y
     if (!is.null(Z)) {
         Z <- wscale(as.matrix(Z), scale = FALSE, w = w)
         QZ <- qr(Z)
@@ -27,6 +34,7 @@
         RDA <- svd(qr.fitted(Q, Y))
         RDA$d <- RDA$d^2
         RDA$u <- sqrt(1/w) * RDA$u
+        RDA$v <- sqrt(1/cw) * RDA$v
         RDA$w <- Y %*% RDA$v %*% diag(1/RDA$d)
         Y <- qr.resid(Q, Y)
     } else {
@@ -35,6 +43,7 @@
     RES <- svd(Y)
     RES$d <- RES$d^2
     RES$u <- sqrt(1/w) * RES$u
+    RES$v <- sqrt(1/cw) * RES$v
     list(RDA = RDA, RES = RES)
 }
 
