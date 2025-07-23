67bfb6ad4734ea659c0583141fd8f0667da57be1
Jari Oksanen <jhoksane@gmail.com>
2025-07-23 13:05:35 +0300

    dbRDA implements db-RDA instead of capscale

--- R/dbRDA.R
+++ R/dbRDA.R
@@ -1,32 +1,40 @@
 #' Distance-based Redundancy Analysis
 #'
-#' Distance-based Redundancy Analysis is a Redundancy Analysis on the
-#' eigenvalue-scaled result of Principal Coordinates Analysis.
-#'
+#' Distance-based Redundancy Analysis is a Redundancy Analysis of
+#' Gower-standardized dissimilarity matrix.
 #'
 #' @param D Dissimilarity structure of dependent data as in \code{\link{PCoA}}.
-#' @param X Model matrix of constraints (can be missing).
-#' @param Z Model matrix of conditions (can be missing).
-#' @return The function returns the same object as \code{\link{pRDA}}.
+#' @param X Model matrix of constraints.
+#' @return The function returns the same object as \code{\link{RDA}}.
 #' @author Jari Oksanen
 #'
-#' @note This function does not actually perform distance-based RDA,
-#'     but a simple variant called \code{\link[vegan]{capscale}} in
-#'     \pkg{vegan}. The function will be fixed ASAP.
-#'
-#' @seealso \code{\link{PCoA}}, \code{\link{pRDA}},
-#' \code{\link[vegan]{capscale}}.
-#' @references Legendre, P. & Anderson, M.J. (1999). Canonical analysis of
-#' principal coordinates: a useful method of constrained ordination for
-#' ecology. \emph{Ecology} 84, 511--525.
+#' #' @seealso \code{\link{PCoA}}, \code{\link{RDA}},
+#' \code{\link[vegan]{dbrda}}.
+#' @references McArdle, B.H. & Anderson, M.J. (2001).  Fitting
+#'     multivariate models to community data: a comment on
+#'     distance-based redundancy analysis.  _Ecology_ 82, 290-297.
 #' @keywords multivariate
 #' @export dbRDA
 `dbRDA` <-
-    function(D, X = NULL, Z = NULL)
+    function(D, X)
 {
-    SOL <- PCoA(D)
-    pos <- SOL$values > sqrt(.Machine$double.eps)
-    Lambda <- SOL$values[pos]
-    Y <- SOL$vectors[, pos] %*% diag(sqrt(Lambda))
-    pRDA(Y, X, Z)
+    M <- as.matrix(D^2)
+    M <- scale(M, scale = FALSE)
+    M <- t(scale(t(M), scale = FALSE))
+    M <- -M/2
+    Q <- qr(scale(X, scale=FALSE))
+    fit <- qr.fitted(Q, M)
+    fit <- qr.fitted(Q, t(fit))
+    RDA <- eigen(fit, symmetric = TRUE)
+    pos <- RDA$values > sqrt(.Machine$double.eps)
+    RDA$values <- RDA$values[pos]
+    RDA$vectors <- RDA$vectors[, pos]
+    RDA$w <- M %*% RDA$vectors %*% diag(1/RDA$values)
+    M <- qr.resid(Q, M)
+    M <- qr.resid(Q, t(M))
+    RES <- eigen(M, symmetric = TRUE)
+    nz <- abs(RES$value) > sqrt(.Machine$double.eps)
+    RES$values <- RES$values[nz]
+    RES$vectors <- RES$vectors[, nz]
+    list(RDA = RDA, Resid = RES)
 }
